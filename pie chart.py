c3b8c20b79e235ad3869fbbd34a54e71a39fa053
Diksha-057 <klsvppcs23dikshahundre@gmail.com>
2025-03-13 12:35:09 +0530

    Add files via upload

---
+++ pie chart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y=np.array([1,2,3,4,5,])
+cars=['ertiga','Creta','Thar','Nexon','Swift']
+plt.title("Pie chart")
+plt.pie(y, labels=cars,startangle=90,autopct="%d")
+plt.legend(cars)
+plt.show()
